33d85fbe7d207faf8628e0f1b464325193e4016a
StephenDogara <maildogarastephen@gmail.com>
2025-06-17 12:55:56 +0100

    Add files via upload

---
+++ account.py
@@ -0,0 +1,14 @@
+class Account:
+    def __init__(self,balance):
+        self.balance = balance
+    
+    def withdraw(self,balance):
+        if amount < self.balance:
+            self.balance -= amount
+            
+    def deposit(self, amount):
+        self.balance += amount
+    
+    def amount(self,amount):
+        self.amount()
+    
---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="Bank App", layout="centered")
+
+st.title("NEO BANK")
+st.header('Welcome to NEO Bank')
+st.write('Your trust is our greatest asset.')
+st.write('At Neo Bank, we are committed to providing you with secure, reliable, and innovative financial solutions to help you grow and succeed. Whether you are saving for the future, managing your everyday finances, or planning for something big—were here to support you every step of the way.')
+st.write('Thank you for choosing us. Welcome to a better banking experience.')
